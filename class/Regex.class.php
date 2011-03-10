5d1952f8a1807fe6d3b22572bc0077334513475a
Scott Bowers <sbbowers@gmail.com>
2011-03-10 09:13:48 -0500

    Added helper Regex methods

--- class/Regex.class.php
+++ class/Regex.class.php
@@ -7,4 +7,20 @@ class Regex
 		preg_match($regex, $subject, $matches);
 		return $matches;
 	}
+
+	public static function select($regex, $subject = null)
+	{
+		return current(self::match($regex, $subject));
+	}
+
+	public static function match_all($regex, $subject = null)
+	{
+		preg_match_all($regex, $subject, $matches);
+		return $matches;
+	}
+		
+	public static function select_all($regex, $subject = null)
+	{
+		return current(self::match_all($regex, $subject));
+	}
 }
