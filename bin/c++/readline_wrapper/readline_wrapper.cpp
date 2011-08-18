ac8f4e7cc1e442ab81a740d2aaa14c315bf2751e
Scott Bowers <sbbowers@gmail.com>
2011-08-18 00:49:38 -0400

    Updated readline_wrapper app
     * Renamed c,read -> readline_wrapper
     * Updated to include mutex signal for i/o
     * Cleaned up code
     * Updated class Readline to use mutex
     * Fixed SIGINT handling
     * Added readline compatible copyright

---
+++ bin/c++/readline_wrapper/readline_wrapper.cpp
@@ -0,0 +1,134 @@
+// c,read.cpp
+//   Used to provide readline support for systems that don't provide the readline php extension
+//
+// Usage: c,read [<question>]
+//      requires tty on stdin, stdout
+//      returns commands on stderr
+//      command history read from file descriptor 3, if available
+//      command history read from file descriptor 4, if available
+//
+// Compile with: `g++ -o c,read c,read.cpp -static-libgcc -static-libstdc++ -lreadline -lncurses -ldl -static`
+// Or: `g++ -o c,read c,read.cpp -lreadline`
+//
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <string>
+#include <signal.h>
+
+int sig_read = 0;
+std::string prompt;
+char * command;
+char tbuffer[8192];
+
+void load_history();
+void load_tab_completion();
+void read_command(FILE * fd_sig, FILE * fd_out);
+void sighandler(int sig);
+
+
+int main(int argc, char *argv[])
+{
+    load_history();
+    load_tab_completion();
+    signal(SIGINT, &sighandler); // Don't die on sigint
+
+    FILE * fd_sig = fdopen(5, "r"); // get signals to read from fd5
+    if(!fd_sig)
+        exit(0);
+
+    FILE * fd_out = fdopen(6, "w"); // output lines on fd6
+    if(!fd_out)
+        exit(0);
+
+    if(argc > 1) // load the prompt if available
+        prompt = argv[1];
+
+    do 
+    {
+        read_command(fd_sig, fd_out);
+    } while(command);
+ 
+    free(command);
+    command = NULL;
+    fclose(fd_sig);
+    fclose(fd_out);
+
+    return 0;
+}
+
+// Read input from stdin and output to fd_out
+// Waits for a line of input as a signal on fd_sig before reading
+// signal used for i/o mutex because this application is designed to share stdout
+void read_command(FILE * fd_sig, FILE * fd_out)
+{
+
+    if(!sig_read)
+    {
+       fgets(tbuffer, 8192, fd_sig); // wait for signal from main process
+       sig_read = 1; 
+    }
+
+    if(ferror(fd_sig) || feof(fd_sig))
+    {
+        free(command);
+        command = NULL;
+    }
+    else
+    {
+        command = readline(prompt.c_str());
+        if(command)
+        {
+            if(fd_out)
+            {
+                fprintf(fd_out, "%s\n", command);
+                fflush(fd_out);
+                sig_read = 0;
+            }
+            else
+            {
+                free(command);
+                command = NULL;
+            }
+
+            if(command[0])
+                add_history(command);
+        }
+    }
+}
+
+void load_history()
+{
+    FILE * fd = fdopen(3, "r");
+    if(fd)
+    {
+        while(fgets(tbuffer, 8192, fd))
+        {
+            tbuffer[strcspn(tbuffer, "\n")] = '\0'; // remove newline
+            add_history(tbuffer);
+        }
+        fclose(fd);
+    }   
+}
+void load_tab_completion()
+{
+    /*
+    FILE * fd;
+    char t[1024];
+    if(fd = fdopen(4, "r"))
+    {
+        while(fgets(t, 1000, fd))
+        {
+            t[strcspn(t, "\n")] = '\0'; // remove newline
+            printf("fd1: %s", t);
+        }
+    }   
+    */  
+}
+
+// Prevent SIGINT and cleans up the terminal
+void sighandler(int sig) 
+{
+    printf("\n%s", prompt.c_str());
+}
--- class/Readline.class.php
+++ class/Readline.class.php
@@ -12,32 +12,39 @@ class Readline
     $history_file,
     $history = array(),
     $descriptorspec = array(
-      0 => STDIN,  
-      1 => STDOUT,  
-      2 => array("pipe", "w"),
-      3 => array("pipe", "r"), // read history from here
-      4 => array("pipe", "r"), // read completion functions from here
+      0 => STDIN,
+      1 => STDOUT,
+      3 => array("pipe", "w"), // read history from here
+      4 => array("pipe", "w"), // read completion functions from here
+      5 => array("pipe", "r+"), // send read signal
+      6 => array("pipe", "w+"), // recieve input
     );
+  CONST SIG_PIPE = 5;
+  CONST COM_PIPE = 6;
 
 
   public function __construct($prompt = '', $history_file = null)
   {
+    $bin = Auto::$FPATH.'/bin/c++/readline_wrapper/readline_wrapper';
     $this->prompt = $prompt;
     $this->history_file = $history_file;
 
-    $this->process = proc_open(dirname(__DIR__).'/bin/c++/c,read/c,read "'.$prompt.'"', $this->descriptorspec, $this->handle);
+    $this->process = proc_open("$bin '$prompt'", $this->descriptorspec, $this->handle);
 
     if($this->history_file)
       $this->read_history();
 
     $this->load_autocompete();
+
+    pcntl_signal(SIGINT, function(){});
   }
 
   public function read()
   {
-    if(is_resource($this->process) && is_resource($this->handle[2]) && !feof($this->handle[2]))
+    fprintf($this->handle[self::SIG_PIPE], "read\n");
+    if(is_resource($this->process) && is_resource($this->handle[self::COM_PIPE]) && !feof($this->handle[self::COM_PIPE]))
     {
-        $this->last_line = fgets($this->handle[2]);
+        $this->last_line = fgets($this->handle[self::COM_PIPE]);
         if($this->last_line)
           $this->history[] = rtrim($this->last_line);
 
@@ -50,13 +57,11 @@ class Readline
   {
     if($this->history_file)
       file_put_contents($this->history_file, implode("\n", array_slice($this->history, -1000)));
-
-      print implode("\n", array_slice($this->history, -1000));
   }
 
   protected function read_history()
   {
-    if($this->history_file)
+    if(file_exists($this->history_file))
     {
       $content = file_get_contents($this->history_file);
       fprintf($this->handle[3], $content);
