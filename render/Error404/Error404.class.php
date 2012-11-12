9bb681300963cb541439895a9ff587c5e7f7e9ce
zepplen <github.com@zepplen.com>
2012-11-12 17:09:21 -0500

    Updated the Error404 renderable to pull paths from Auto instead of Resolve. Updated location logic to fix functionality.

--- render/Error404/Error404.class.php
+++ render/Error404/Error404.class.php
@@ -14,19 +14,20 @@ class Error404 extends Controller
 		$ret = array_pad(explode('/', $_GET['__route'], 3),3, null);
 		$controller = $ret[0];
 		$search = array();
-		$search[] = Resolve::$APATH.'/render/';
-		$search[] = Resolve::$FPATH.'/render/';
+		$search[] = Auto::$APATH.'/render/';
+		$search[] = Auto::$FPATH.'/render/';
 		foreach($search as $path)
 		{
 			$found = $this->isearch_dir($path, $controller);
 			if($found)
 			{
-				$file_found = $this->isearch_dir($path.$found, $ret[1]);
+				$file_found = $this->isearch_dir($path.$found, $ret[1].'.php');
 				if($file_found)
 				{
+					$file_found = substr($file_found, 0, -4);
 					$new_uri = $_SERVER['REDIRECT_URI_PREFIX'].'/'.$found.'/'.$file_found;
 					header( "HTTP/1.1 301 Moved Permanently" );
-					header( "Location: $new_uri" ); 
+					header( "Location: /$new_uri" ); 
 					break;
 				}
 			}
